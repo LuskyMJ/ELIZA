56a39615308462cf0d668300c88fcf81fcdb45a9
Magnus Jacobsen <mosquito1306@gmail.com>
2022-04-25 09:03:51 +0200

    Not working

---
+++ script.js
@@ -0,0 +1,6 @@
+userInput = document.getElementById("userInput");
+userSubmit = document.getElementById("userSubmit");
+
+userInput.addEventListener("click", () => {
+    console.log(userInput.value);   
+});
